cdeb72196e0f85f13a809fee9f24457b9da5cc7c
Drage <bydawn96@gmail.com>
2026-06-09 17:10:02 -0500

    Se creo este commit por si acaso xd

--- entregas/camilo-galan-proyecto05/Base de Datos/cinema_db.sql
+++ entregas/camilo-galan-proyecto05/Base de Datos/cinema_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-06-2026 a las 23:57:21
+-- Tiempo de generación: 10-06-2026 a las 00:09:08
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
